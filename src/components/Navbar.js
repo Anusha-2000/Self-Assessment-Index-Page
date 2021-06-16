1c3c7efea226553238dfc3f3deeddd113b7932b1
Anusha-2000 <63717792+Anusha-2000@users.noreply.github.com>
2021-06-16 09:29:36 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from "react";
-import logo from "../logo.png";
+//import logo from "../logo.png";
 //REACT FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Signin from "./Signin.js";
 
 
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light">
     <div className="container">
 
-      <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."  height="50"/></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <FontAwesomeIcon icon={ faBars} style={{ color: "#fff" }}/>
       </button>
@@ -28,7 +28,7 @@ const Navbar = () => {
             <a className="nav-link" href="#">Services</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Contact us</a>
+            <a className="nav-link" href={ Signin }>Login</a>
           </li>
           
         </ul>
